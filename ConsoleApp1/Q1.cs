2c739ad8d033318917161fc8e8fe387e46d9b991
Gauravpachauri <gaurav.pachauri359190@gmail.com>
2025-10-24 21:02:34 +0530

    done 2 questions

---
+++ ConsoleApp1/Q1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Q1
+{
+    static void Main()
+    {
+        int[] transactions = { 200, -150, 340, 500, -100 };
+        int sum = 0;
+
+        for (int i = 0; i < transactions.Length; i++)
+        {
+            sum += transactions[i];
+        }
+
+        Console.WriteLine("Total sum of all transactions: " + sum);
+    }
+}
