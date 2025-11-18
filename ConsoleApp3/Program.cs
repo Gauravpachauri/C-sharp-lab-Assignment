857ec90ee9cc4289c431bc6fe275726b4f33a976
Gauravpachauri <gaurav.pachauri359190@gmail.com>
2025-11-18 14:27:56 +0530

    question 2 done

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ObsoleteDemo
+{
+    class Program
+    {
+        // Old method marked as obsolete
+        [Obsolete("Use NewGreet() instead. OldGreet is deprecated!", false)]
+        static void OldGreet()
+        {
+            Console.WriteLine("Hello from the OLD method.");
+        }
+
+        // New recommended method
+        static void NewGreet()
+        {
+            Console.WriteLine("Hello from the NEW method.");
+        }
+
+        static void Main(string[] args)
+        {
+            OldGreet();    // This will show a WARNING in Visual Studio!
+            NewGreet();    // No warning, use this one
+            Console.ReadLine();
+        }
+    }
+}
